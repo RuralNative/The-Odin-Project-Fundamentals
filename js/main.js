e848ab59e3b386256d9e1c684941a518c00e3bf0
RuralNative <johnberlinleonor@yahoo.com>
2024-09-20 08:14:06 +0800

    Added Code to Initialize 16x16 Grid

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 let initializeGrid = () => {
+    // Initialize Grid Container with Assigned Class/Style
     const gridContainer = document.createElement("div")
     gridContainer.classList.add("grid-container")
+    document.body.appendChild(gridContainer)
+    // Create 16 x 16 number of Divs within Container and ensure they are sized in a way that ensures 16 x 16 container layout
+    for (i = 0; i < (16*16); i++) {
+        let gridSquare = document.createElement("div")
+        gridSquare.classList.add("grid-square")
+        gridContainer.appendChild(gridSquare)
+    }
 }
